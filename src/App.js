3361a4b6c9f2c08d15ec6538307f5be44ca348be
Hiroki Harry Ota <mikan3rd@users.noreply.github.com>
2017-07-04 17:03:23 +0900

    cssの修正

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import './App.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
+        <h1>todoアプリを作ってみた</h1>
         <Form/>
       </div>
     );
--- src/Form.js
+++ src/Form.js
@@ -4,12 +4,11 @@ import './form.css';
 export default class Form extends Component {
   render() {
     return (
-      <div>
+      <div className="form">
         <form>
           <input type="text" placeholder="タイトルを入力"/><br/>
           <textarea placeholder="説明を入力"></textarea><br/>
           <button type="submit">送信</button>
-          <p className="test">テストォォォォォ</p>
         </form>
       </div>
     );
