9826583a12529adde93ebdf98c6d9cc97f07014c
Hiroki Harry Ota <mikan3rd@users.noreply.github.com>
2017-07-04 16:47:04 +0900

    cssの適用

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import './form.css';
 
-export default class From extends Component {
+export default class Form extends Component {
   render() {
     return (
       <div>
@@ -8,6 +9,7 @@ export default class From extends Component {
           <input type="text" placeholder="タイトルを入力"/><br/>
           <textarea placeholder="説明を入力"></textarea><br/>
           <button type="submit">送信</button>
+          <p className="test">テストォォォォォ</p>
         </form>
       </div>
     );
