439d8da695376cf1b35b5a048a49d5c34ded676e
Hiroki Harry Ota <mikan3rd@users.noreply.github.com>
2017-07-06 16:12:39 +0900

    削除でtodo完了数が減らないよう記述

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      todos: []
+      todos: [],
+      deleteNum: 0
     };
   }
 
@@ -51,22 +52,33 @@ export default class App extends Component {
 
   deleteTodoState(clickTodo) {
     const todos = this.state.todos.slice();
+    const deleteNum = this.state.deleteNum + 1 ;
     todos.splice(clickTodo, 1);
     this.setState({ todos });
+    this.setState({ deleteNum });
   }
 
   render() {
     let undoneNum = 0;
+    let doneNum = 0;
+    let level = 0;
+
     this.state.todos.forEach((todo) => {
       if (todo.done === false) {
         undoneNum++;
       }
     });
 
+    this.state.todos.forEach((todo) => {
+      if (todo.done === true) {
+        doneNum++;
+      }
+    });
+
     return (
       <div className="app">
         <h1>todoアプリを作ってみた</h1>
-        <h2>残り：{undoneNum}</h2>
+        <h2>完了：{doneNum + this.state.deleteNum}　　残り：{undoneNum}</h2>
         <Form onSubmit={this.handleSubmit.bind(this)} />
         <TodoList
           todos={this.state.todos}
